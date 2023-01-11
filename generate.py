c0cd4c599eaa7d8983e1d7a76df0aad9c033681e
rerdakos <robmets11@gmail.com>
2023-01-11 14:50:47 -0600

    Create torso

--- generate.py
+++ generate.py
@@ -2,15 +2,20 @@ from operator import length_hint
 from re import X
 import pyrosim.pyrosim as pyrosim
 
-pyrosim.Start_SDF("world.sdf")
 
-length = 1
-width = 1
-height = 1
-x = 0
-y = 0
-z = 0.5*height
+def Create_World():
+    pyrosim.Start_SDF("world.sdf")
 
-pyrosim.Send_Cube(name="Box", pos=[x,y,z] , size=[length,width,height])
+    pyrosim.Send_Cube(name="Box", pos=[0,0,0.5] , size=[1,1,1])
 
-pyrosim.End()
+    pyrosim.End()
+
+def Create_Robot():
+    pyrosim.Start_URDF("body.urdf")
+    
+    pyrosim.Send_Cube(name="Box", pos=[0,0,0.5] , size=[1,1,1])
+
+    pyrosim.End()
+
+Create_World()
+Create_Robot()
