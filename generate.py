bba91a239b03e8cdeac70aff45d4e300813d3779
rerdakos <robmets11@gmail.com>
2023-01-11 14:44:49 -0600

    Minimal World made

--- generate.py
+++ generate.py
@@ -2,7 +2,7 @@ from operator import length_hint
 from re import X
 import pyrosim.pyrosim as pyrosim
 
-pyrosim.Start_SDF("boxes.sdf")
+pyrosim.Start_SDF("world.sdf")
 
 length = 1
 width = 1
@@ -11,15 +11,6 @@ x = 0
 y = 0
 z = 0.5*height
 
-
-for i in range(10):
-    pyrosim.Send_Cube(name="Box", pos=[x,y,z] , size=[length,width,height])
-
-    z += 0.5*height
-    length = 0.9*length
-    width = 0.9*width
-    height = 0.9*height
-    z += 0.5*height
-    
+pyrosim.Send_Cube(name="Box", pos=[x,y,z] , size=[length,width,height])
 
 pyrosim.End()
