a321fb2d51fd1e7227ac9e924e99478f9badd6ae
rerdakos <robmets11@gmail.com>
2023-01-12 11:24:11 -0600

    Made three-link, two-joint robot

--- simulate.py
+++ simulate.py
@@ -8,6 +8,9 @@ p.setAdditionalSearchPath(pybullet_data.getDataPath())
 p.setGravity(0,0,-9.8)
 
 planeId = p.loadURDF("plane.urdf")
+robotId = p.loadURDF("body.urdf")
+
+
 
 p.loadSDF("world.sdf")
 
