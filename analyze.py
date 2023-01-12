156b2ef6ec5639163087a3058bd9985489494de7
rerdakos <robmets11@gmail.com>
2023-01-12 13:12:34 -0600

    add visualization for sensor data
    also added sensor to front leg

--- analyze.py
+++ analyze.py
@@ -1,2 +1,14 @@
-import numpy as numpy
+import numpy as np
+import matplotlib.pyplot as plt
 
+backLegSensorValues = np.load(r'C:\Users\robme\OneDrive\Desktop\Artificial Life\data\backLegSensorValues.npy')
+frontLegSensorValues = np.load(r'C:\Users\robme\OneDrive\Desktop\Artificial Life\data\frontLegSensorValues.npy')
+
+plt.plot(backLegSensorValues, label="Back Leg", linewidth=3)
+plt.plot(frontLegSensorValues, label="Front Leg")
+
+#print(backLegSensorValues)
+#print(frontLegSensorValues)
+
+plt.legend()
+plt.show()
--- simulate.py
+++ simulate.py
@@ -2,7 +2,7 @@ import pybullet as p
 import time
 import pybullet_data
 import pyrosim.pyrosim as pyrosim
-import numpy as numpy
+import numpy as np
 
 physicsClient = p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
@@ -17,16 +17,24 @@ p.loadSDF("world.sdf")
 pyrosim.Prepare_To_Simulate(robotId)
 
 t = 100 #number of iterations
-backLegSensorValues = numpy.zeros(t)
+backLegSensorValues = np.zeros(t)
+frontLegSensorValues = np.zeros(t)
 
 
 for i in range(t):
     p.stepSimulation()
+
     backLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
+    frontLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("FrontLeg")
     #print(backLegTouch)
+    #print(frontLegTouch)
+
     time.sleep(1/60)
     #print(i)
 p.disconnect()
 
-numpy.save(r'C:\Users\robme\OneDrive\Desktop\Artificial Life\data\backlegvals.npy',backLegSensorValues)
-print(backLegSensorValues)
+np.save(r'C:\Users\robme\OneDrive\Desktop\Artificial Life\data\backLegSensorValues.npy',backLegSensorValues)
+np.save(r'C:\Users\robme\OneDrive\Desktop\Artificial Life\data\frontLegSensorValues.npy',frontLegSensorValues)
+
+#print(backLegSensorValues)
+#print(frontLegSensorValues)
