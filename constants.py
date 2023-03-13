014e4cfc451de93e5571f1edc297b0d196b310f0
rerdakos <robmets11@gmail.com>
2023-03-13 14:43:16 -0500

    directions

--- constants.py
+++ constants.py
@@ -1,13 +1,17 @@
 import numpy as np
 import random
 
+# VARIABLES TO CHANGE
 t = 1000 # Time for each simulation
 
 numberOfGenerations = 10 # Number of Generations
 populationSize = 3 # Size of Population
 
+motorJointRange = 0.2 # Range for motors
+
+# CONSTANT PARAMETERS
 nodes = 4 # Number of node locations for limb generation
 numSensorNeurons = nodes*2 # Number of possible sensor locations
 numMotorNeurons = nodes*2 # Number of possible motor locations
 
-motorJointRange = 0.2 # Range for motors
+
